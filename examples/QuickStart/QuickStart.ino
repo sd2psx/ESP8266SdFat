94094c090a0f10734e608158f6e85f5f4553c345
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-12-21 12:35:22 -0800

    Make examples build by default

--- examples/QuickStart/QuickStart.ino
+++ examples/QuickStart/QuickStart.ino
@@ -9,7 +9,7 @@ using namespace sdfat;
 
 // SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
 // 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
-#define SD_FAT_TYPE 3
+#define SD_FAT_TYPE 1
 //
 // Set DISABLE_CHIP_SELECT to disable a second SPI device.
 // For example, with the Ethernet shield, set DISABLE_CHIP_SELECT
